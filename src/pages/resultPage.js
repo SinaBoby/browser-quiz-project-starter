c17b282bc182a79a39583614e47e2132b14a9c09
Fedor Babikov <fed.babikov@protonmail.com>
2022-01-29 21:13:28 +0100

    fixed time score in result page

--- src/pages/resultPage.js
+++ src/pages/resultPage.js
@@ -3,29 +3,34 @@
 import { createResultElement } from '../views/resultView.js';
 import { createReferenceElement } from '../views/referenceView.js';
 import { quizData } from '../data.js';
+import { REFERENCE_LIST_ID } from '../constants.js';
 
 export const resultPage = (userInterface, refresh = '') => {
   
   let resultElement;
   
   if (refresh === 'result') {
-    resultElement = createResultElement(sessionStorage.correctSum, sessionStorage.wrongSum);
+    resultElement = createResultElement(
+      sessionStorage.correctSum,
+      sessionStorage.wrongSum,
+      sessionStorage.timeScore);
   } else {
-    resultElement = createResultElement(quizData.correctSum, quizData.wrongSum);
+    resultElement = createResultElement(quizData.correctSum, quizData.wrongSum, quizData.timeScore);
     sessionStorage.setItem('result', 'true');
     sessionStorage.setItem('correctSum', `${quizData.correctSum}`);
     sessionStorage.setItem('wrongSum', `${quizData.wrongSum}`);
+    sessionStorage.setItem('timeScore', `${quizData.timeScore}`);
     }
 
     userInterface.appendChild(resultElement);
 
-    const referenceListElement = document.getElementById(REFERENCE_LIST_ID);
+    // const referenceListElement = document.getElementById(REFERENCE_LIST_ID);
 
-    for (const {links} of quizData.questions) {
-      for (const link of links) {
-          const referenceElement = createReferenceElement(link.text, link.href);
-          referenceListElement.appendChild(referenceElement);  
-        }
+    // for (const {links} of quizData.questions) {
+    //   for (const link of links) {
+    //       const referenceElement = createReferenceElement(link.text, link.href);
+    //       referenceListElement.appendChild(referenceElement);  
+    //     }
         
-      }
+    //   }
 };
--- src/views/resultView.js
+++ src/views/resultView.js
@@ -2,7 +2,7 @@
 
 import { quizData } from '../data.js';
 
-export const createResultElement = (correctSum, wrongSum) => {
+export const createResultElement = (correctSum, wrongSum, timeScore) => {
   const element = document.createElement('div');
   element.id = "result-interface";
   element.innerHTML = String.raw`
@@ -10,7 +10,7 @@ export const createResultElement = (correctSum, wrongSum) => {
         <p>You have finished the quiz about JavaScript</p>
         <p>Correct answers: ${correctSum}</p>
         <p>Wrong answers: ${wrongSum}</p>
-        <p>Fast answering: ${quizData.timeScore}/10 points</p>
+        <p>Fast answering: ${timeScore}/10 points</p>
         <p>For more about JavaScript, visit:</p>
         <a href="https://www.w3schools.com/" class="link reference-link">https://w3schools.com</a>
         <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" class="link reference-link">MDN-WebDocs/javascript</a>
